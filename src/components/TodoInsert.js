67e96486ae025a5c4344f58767730b2a0c94734a
minh0518 <rubics@naver.com>
2022-05-19 01:58:42 +0900

    [ADD]TodoInsert상태값

--- src/components/TodoInsert.js
+++ src/components/TodoInsert.js
@@ -1,10 +1,21 @@
+import { useState, useCallback } from 'react'
 import { MdAdd } from 'react-icons/md'
 import './TodoInsert.scss'
 
 const TodoInsert = () => {
+  const [value, setValue] = useState('')
+
+  const onChange = useCallback((e) => {
+    setValue(e.target.value)
+  }, [])
+
   return (
     <form className="TodoInsert">
-      <input placeholder="할 일을 입력하세요"></input>
+      <input
+        placeholder="할 일을 입력하세요"
+        value={value}
+        onChange={onChange}
+      />
       <button type="submit">
         <MdAdd />
       </button>
@@ -12,5 +23,4 @@ const TodoInsert = () => {
   )
 }
 
-
-export default TodoInsert;
+export default TodoInsert
